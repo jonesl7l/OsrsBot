91caa93cddfb4d9d9169104dcac944b5d5ec4d50
William Jones <william.jones@rha.co.uk>
2021-08-17 13:57:22 +0100

    first commit

--- src/main.kt
+++ src/main.kt
@@ -1,17 +1,9 @@
 import com.epicbot.api.shared.GameType
-import com.epicbot.api.shared.entity.NPC
-import com.epicbot.api.shared.entity.details.Locatable
 import com.epicbot.api.shared.script.LoopScript
 import com.epicbot.api.shared.script.ScriptManifest
-import com.epicbot.api.shared.util.time.Time
 import combat.KillCommand
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.runBlocking
-import utils.Consts
 import utils.printMsg
-import kotlin.coroutines.coroutineContext
+import kotlin.random.Random
 
 @ScriptManifest(name = "KillCommand", gameType = GameType.OS)
 class main : LoopScript() {
@@ -30,13 +22,14 @@ class main : LoopScript() {
 
     override fun loop(): Int {
         printMsg("loop")
-        return LOOP_COUNT
+        killCommand.pollKillCommand()
+        return LOOP_TIMER
     }
 
     //endregion
 
     companion object {
-        private const val LOOP_COUNT = 2
+        private val LOOP_TIMER by lazy { Random.nextInt(328, 636) }
         private const val AREA_DIMENSIONS = 9 //squared
     }
 }
